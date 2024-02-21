74b3ee664298fe72e7db80f7bc91c498185f49f9
Boomika <111379827+Boomika-Suresh@users.noreply.github.com>
2024-02-21 16:02:13 +0530

    String Concatenation.java

---
+++ Programs/String Concatenation.java
@@ -0,0 +1,9 @@
+class main {
+    public static void main(String[] args) {
+        String s1= "Apple";
+        String s2= "Pie";
+        String s3="";
+        s3 = s1+s2;
+        System.out.print(s3);
+    }
+}
